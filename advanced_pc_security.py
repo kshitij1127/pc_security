4028aae7e5e08c2882e968fe1f52ec6b82378197
kshitij <63649035+kshitij1127@users.noreply.github.com>
2022-01-31 18:42:44 +0530

    Add files via upload

---
+++ advanced_pc_security.py
@@ -0,0 +1,46 @@
+from fileinput import filename
+from os import access
+from tracemalloc import start
+import cv2
+import dropbox
+import time
+import random
+
+start_time = time.time()
+
+def take_snap():
+    number = random.randint(1, 100)
+    video_capture_object = cv2.VideoCapture(0)
+    result = True
+    while (result):
+        ret, frame = video_capture_object.read()
+        filename = "this_person_is_using_the_pc" + str(number) + ".png"
+        cv2.imwrite(filename, frame)
+        start_time = time.time
+        result = False
+
+    
+    video_capture_object.release()
+    cv2.destroyAllWindows()
+    print("snapshot taken. Check your cloud for the image.")
+    return filename
+
+def upload_snap(imgname):
+    access_token = 'hlbnIgnFHW4AAAAAAAAAATu_CwD2lWmAPlEQvOIUAopaTzlkDr3tUdzY_A80kS0z'
+    file = imgname
+    file_from = file
+    file_to = '/who_is_using_this_pc/' + imgname
+    dbx = dropbox.Dropbox(access_token)
+    with open(file_from, 'rb') as f:
+        dbx.files_upload(f.read(), file_to, mode=dropbox.files.WriteMode.overwrite)
+        print("uploaded")
+    
+
+def main():
+    while (True):
+        if((time.time() - start_time) >= 10):
+            name = take_snap()
+            upload_snap(name)
+
+
+main()
